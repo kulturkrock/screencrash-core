e171f7d0c5cd96a842a5b4662389c25de3722b82
Matz Larsson <matzlarsson94@gmail.com>
2021-12-26 15:04:11 +0100

    Minor improvements to typing

--- src/actors/internal.py
+++ src/actors/internal.py
@@ -1,3 +1,5 @@
+from typing import List, Dict
+from opus import Asset
 from .actor import Actor
 
 class InternalActor(Actor):
@@ -5,6 +7,8 @@ class InternalActor(Actor):
     def __init__(self):
         super().__init__("internal")
 
-    def handle_action(self, cmd, assets, params):
+    def handle_action(self, cmd: str, assets: List[Asset], params: Dict[str, str]):
         if cmd == "print":
             print(params.get("text", "[print command found no text to print]"))
+        else:
+            super().handle_action(cmd, assets, params)
